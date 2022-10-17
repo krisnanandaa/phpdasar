05ac8e22cf9a50c7333cdecda04c69ffa9bc0d57
krisnanandaa <priyananda65@gmail.com>
2022-10-17 14:25:39 +0700

    latihan4

---
+++ latihan3/latihan4.php
@@ -0,0 +1,8 @@
+<?php
+    echo "<h1> Praktikum Menggunkan PHP </h1>";
+    echo "<a href = 'latihan1.php'>latihan 1 (Unorder List)</a><br>";
+    echo "<a href = 'latihan1a.php'>latihan 1 (Table)</a><br>";
+    echo "<a href = 'latihan2.php'>latihan 2 </a><br>";
+    echo "<a href = 'latihan3.php'>latihan 3 </a>";
+
+?>
